e0d2c74f5c2186cb0f86db62adea1733b271b59e
Na <lette1394+github@gmail.com>
2018-10-22 03:30:55 +0900

    ADD score board

--- src/main/java/BowlingScore/Main.java
+++ src/main/java/BowlingScore/Main.java
@@ -9,23 +9,144 @@ public class Main {
         try (final BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
             in.readLine();
 
+            System.out.print(" ");
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%8s", "________"));
+            System.out.print(String.format("%9s", "___________"));
+            System.out.println();
+
             System.out.print("|");
-            System.out.print(String.format("%2s %s %2s", "", "1", ""));
+            System.out.print(String.format("%3s%s%3s", "___", "1", "___"));
             System.out.print("|");
-            System.out.print(String.format("%2s %s %2s", "", "2", ""));
+            System.out.print(String.format("%3s%s%3s", "___", "2", "___"));
             System.out.print("|");
-            System.out.print(String.format("%2s %s %2s", "", "3", ""));
+            System.out.print(String.format("%3s%s%3s", "___", "3", "___"));
             System.out.print("|");
-            System.out.print(String.format("%2s %s %2s", "", "4", ""));
+            System.out.print(String.format("%3s%s%3s", "___", "4", "___"));
             System.out.print("|");
-            System.out.print(String.format("%2s %s %2s", "", "5", ""));
+            System.out.print(String.format("%3s%s%3s", "___", "5", "___"));
             System.out.print("|");
-            System.out.print(String.format("%2s %s %2s", "", "6", ""));
+            System.out.print(String.format("%3s%s%3s", "___", "6", "___"));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "___", "7", "___"));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "___", "8", "___"));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "___", "9", "___"));
+            System.out.print("|");
+            System.out.print(String.format("%4s%s%4s", "____", "10", "_____"));
             System.out.print("|");
 
             System.out.println();
 
+            //line break
 
+            System.out.print("|");
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%8s", "   |   |"));
+            System.out.print(String.format("%9s", "   |   |   |"));
+            System.out.println();
+
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "9", "|", "/"));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "X", "|", " "));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "X", "|", " "));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "9", "|", "8"));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "9", "|", "-"));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "9", "|", "8"));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "9", "|", "8"));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "9", "|", "8"));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %-2s", "9", "|", "8"));
+            System.out.print("|");
+            System.out.print(String.format("%2s %s %1s %s %1s ", "a", "|", "b", "|", "c"));
+            System.out.print("|");
+            System.out.println();
+
+            System.out.print("|");
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%8s", "___|___|"));
+            System.out.print(String.format("%9s", "___|___|___|"));
+            System.out.println();
+
+            // line break;
+
+            System.out.print("|");
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%8s", "|"));
+            System.out.print(String.format("%12s", "|"));
+            System.out.println();
+
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "1", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "2", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "3", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "4", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "5", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "6", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "7", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "8", ""));
+            System.out.print("|");
+            System.out.print(String.format("%3s%s%3s", "", "9", ""));
+            System.out.print("|");
+            System.out.print(String.format("%4s%s%5s", "", "10", ""));
+            System.out.print("|");
+            System.out.println();
+
+            System.out.print("|");
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%8s", "_______|"));
+            System.out.print(String.format("%9s", "___________|"));
+            System.out.println();
 
         } catch (Exception e) {
             System.err.println(e.toString());

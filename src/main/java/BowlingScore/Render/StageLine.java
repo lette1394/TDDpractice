2750522ac6b8b915ea469bb35c857b12d68a9a4e
Na <lette1394+github@gmail.com>
2018-10-22 17:07:52 +0900

    PASS StageLine with custom vertical delimiter

--- src/main/java/BowlingScore/Render/StageLine.java
+++ src/main/java/BowlingScore/Render/StageLine.java
@@ -5,18 +5,13 @@ import java.util.stream.Stream;
 public class StageLine {
     private static Integer startNumber = 1;
 
-    public static String render(Integer limit, Integer blockWidth) {
+    public static String render(Integer limit, Integer blockWidth, String delimiter) {
         return Stream.iterate(startNumber, i -> i + 1)
                 .limit(limit)
                 .map(Stage::stage)
-                .map(stage -> {
-                    String verticalLine = "|";
-                    String stageLine = stage.render(blockWidth);
-
-                    return verticalLine + stageLine;
-                })
+                .map(stage -> delimiter + stage.render(blockWidth))
                 .reduce((a, b) -> a + b)
-                .map(last -> last + "|")
+                .map(last -> last + delimiter)
                 .orElse("");
     }
 }
--- src/test/java/BowlingScore/StageLineTest.java
+++ src/test/java/BowlingScore/StageLineTest.java
@@ -11,7 +11,7 @@ class StageLineTest {
     void render_9_stages() throws Exception {
         Integer blockWidth = 3;
         Integer limit = 9;
-        String ret = StageLine.render(limit, blockWidth);
+        String ret = StageLine.render(limit, blockWidth, "|");
         String expected = "|___1___|___2___|___3___|___4___|___5___|___6___|___7___|___8___|___9___|";
 
         assertThat(ret).isEqualTo(expected);
@@ -21,7 +21,7 @@ class StageLineTest {
     void render_10_stages() throws Exception {
         Integer blockWidth = 3;
         Integer limit = 10;
-        String ret = StageLine.render(limit, blockWidth);
+        String ret = StageLine.render(limit, blockWidth, "|");
         String expected = "|___1___|___2___|___3___|___4___|___5___|___6___|___7___|___8___|___9___|____10_____|";
 
         assertThat(ret).isEqualTo(expected);
@@ -31,8 +31,19 @@ class StageLineTest {
     void render_more_than_10_stages() throws Exception {
         Integer blockWidth = 3;
         Integer limit = 20;
-        String ret = StageLine.render(limit, blockWidth);
+        String ret = StageLine.render(limit, blockWidth, "|");
         String expected = "|___1___|___2___|___3___|___4___|___5___|___6___|___7___|___8___|___9___|____10_____|____11_____|____12_____|____13_____|____14_____|____15_____|____16_____|____17_____|____18_____|____19_____|____20_____|";
+
+        assertThat(ret).isEqualTo(expected);
+    }
+
+    @Test
+    void render_with_custom_vertical_delimiter() throws Exception {
+        Integer blockWidth = 3;
+        Integer limit = 5;
+        String customDelimiter = ":";
+        String ret = StageLine.render(limit, blockWidth, customDelimiter);
+        String expected = ":___1___:___2___:___3___:___4___:___5___:";
 
         assertThat(ret).isEqualTo(expected);
     }

d92f0d0a294d73fd993b4efba2a7fea8cf28fe04
Na <lette1394+github@gmail.com>
2018-10-23 01:12:23 +0900

    PASS TEST for MiddleLine

--- src/main/java/BowlingScore/Render/MiddleLine.java
+++ src/main/java/BowlingScore/Render/MiddleLine.java
@@ -1,11 +1,38 @@
 package BowlingScore.Render;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MiddleLine {
-    public static String render() {
+    public static String render(int limit) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|");
+        for (int i = 0; i < limit-1; i++) {
+            sb.append(String.format("%s", "   |   |"));
+        }
+        sb.append(String.format("%s", "   |   |   |"));
+        sb.append("\n");
 
 
-        return "|   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n" +
-                "| 9 | / | X |   | X |   | 9 | 8 | 9 | - | 9 | 8 | 9 | 8 | 9 | 8 | 9 | 8 | a | b | c |\n" +
-                "|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|\n";
+        List<String> scores = Arrays.asList("9", "/", "X", " ", "X", " ", "9", "8", "9", "-", "9", "8", "9", "8", "9", "8", "9", "8");
+        sb.append("|");
+        for (int i = 0; i < limit-1; i++) {
+            sb.append(String.format(" %s %s %s ", scores.get(i * 2), "|", scores.get(i * 2 + 1)));
+            sb.append("|");
+        }
+        sb.append(String.format(" %s %s %s %s %s ", "a", "|", "b", "|", "c"));
+        sb.append("|");
+        sb.append("\n");
+
+
+        sb.append("|");
+        for (int i = 0; i < limit-1; i++) {
+            sb.append(String.format("%s", "___|___|"));
+        }
+        sb.append(String.format("%s", "___|___|___|"));
+        sb.append("\n");
+
+        return sb.toString();
     }
 }
--- src/test/java/BowlingScore/MiddleLineTest.java
+++ src/test/java/BowlingScore/MiddleLineTest.java
@@ -13,7 +13,19 @@ public class MiddleLineTest {
                         "| 9 | / | X |   | X |   | 9 | 8 | 9 | - | 9 | 8 | 9 | 8 | 9 | 8 | 9 | 8 | a | b | c |\n" +
                         "|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|\n";
 
-        String ret = MiddleLine.render();
+        String ret = MiddleLine.render(10);
+
+        assertThat(ret).isEqualTo(expected);
+    }
+
+    @Test
+    void render_middle_line_in_5() throws Exception {
+        String expected =
+                "|   |   |   |   |   |   |   |   |   |   |   |\n" +
+                        "| 9 | / | X |   | X |   | 9 | 8 | a | b | c |\n" +
+                        "|___|___|___|___|___|___|___|___|___|___|___|\n";
+
+        String ret = MiddleLine.render(5);
 
         assertThat(ret).isEqualTo(expected);
     }

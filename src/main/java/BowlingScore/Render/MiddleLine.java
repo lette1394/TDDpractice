f2e142b65a9e0c5263b59fdc1348beb4a7e647ca
Na <lette1394+github@gmail.com>
2018-10-23 15:12:07 +0900

    RFT MiddleLine

--- src/main/java/BowlingScore/Render/MiddleLine.java
+++ src/main/java/BowlingScore/Render/MiddleLine.java
@@ -3,32 +3,23 @@ package BowlingScore.Render;
 import BowlingScore.StageScore;
 
 import java.util.Collection;
-
-import static BowlingScore.Render.Middle.*;
+import java.util.function.Function;
 
 public class MiddleLine {
     public static String render(Collection<StageScore> scoreList) {
+
+        return String.join("\n",
+                renderLine(Middle::renderCeiling, scoreList),
+                renderLine(Middle::renderScore, scoreList),
+                renderLine(Middle::renderFloor, scoreList));
+    }
+
+    private static String renderLine(Function<StageScore, String> mapper, Collection<StageScore> scoreList) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("|");
         scoreList.forEach(score -> {
-            sb.append(renderCeiling(score));
-            sb.append("|");
-        });
-        sb.append("\n");
-
-
-        sb.append("|");
-        scoreList.forEach(score -> {
-            sb.append(renderScore(score));
-            sb.append("|");
-        });
-        sb.append("\n");
-
-
-        sb.append("|");
-        scoreList.forEach(score -> {
-            sb.append(renderFloor(score));
+            sb.append(mapper.apply(score));
             sb.append("|");
         });
 

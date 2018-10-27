ed77497e9b8da434145018d461990ed62dbef8e8
Na <lette1394+github@gmail.com>
2018-10-27 22:02:46 +0900

    RTF MiddleLine

--- src/main/java/BowlingScore/Render/MiddleLine.java
+++ src/main/java/BowlingScore/Render/MiddleLine.java
@@ -2,7 +2,7 @@ package BowlingScore.Render;
 
 import BowlingScore.StageScore;
 
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -10,34 +10,30 @@ import java.util.stream.Stream;
 
 public class MiddleLine {
     public static String render(List<RenderContext> contextList, List<StageScore> scoreList) {
-        List<Middle> middleList = Stream.iterate(0, i->i+1)
+        List<Middle> middleList = Stream.iterate(0, i -> i + 1)
                 .limit(contextList.size())
                 .map(i -> Middle.middle()
                         .setRenderContext(contextList.get(i))
                         .setStageScore(scoreList.get(i)))
                 .collect(Collectors.toList());
 
+        List<Function<Middle, String>> mappers = Arrays.asList(
+                Middle::renderCeiling,
+                Middle::renderContents,
+                Middle::renderFloor
+        );
+
+        return mappers.stream()
+                .map(mapper -> _render(middleList, mapper))
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static String _render(List<Middle> middleList, Function<Middle, String> mapper) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("|");
         for (Middle middle : middleList) {
-            sb.append(middle.renderCeiling());
-            sb.append("|");
-        }
-
-        sb.append("\n");
-
-        sb.append("|");
-        for (Middle middle : middleList) {
-            sb.append(middle.renderContents());
-            sb.append("|");
-        }
-
-        sb.append("\n");
-
-        sb.append("|");
-        for (Middle middle : middleList) {
-            sb.append(middle.renderFloor());
+            sb.append(mapper.apply(middle));
             sb.append("|");
         }
 

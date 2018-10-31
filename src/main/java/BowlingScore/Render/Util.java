f8cc7c55d233e746cda721b0f06f1a6ea28f4d42
Na <lette1394+github@gmail.com>
2018-11-01 02:18:47 +0900

    ADD mergeStage method to Util

--- src/main/java/BowlingScore/Render/Util.java
+++ src/main/java/BowlingScore/Render/Util.java
@@ -1,5 +1,8 @@
 package BowlingScore.Render;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Util {
@@ -17,6 +20,32 @@ public class Util {
     }
 
     static String mergeStage(String left, String right) {
+        String delimiter =
+                "_\n" +
+                "|\n" +
+                "|\n" +
+                "|\n" +
+                "|\n" +
+                "|\n" +
+                "|\n" +
+                "|";
 
+        List<String> targets = Arrays.asList(left, delimiter, right);
+
+        return targets.stream().reduce(Util::mergeStageInner).orElse("");
+    }
+
+    private static String mergeStageInner(String left, String right) {
+        List<String> leftLines = splitByLine(left);
+        List<String> rightLines = splitByLine(right);
+
+        return Stream.iterate(0, i -> i + 1)
+                .limit(leftLines.size())
+                .map(i -> leftLines.get(i) + rightLines.get(i))
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static List<String> splitByLine(String str) {
+        return Arrays.asList(str.split("\n"));
     }
 }
